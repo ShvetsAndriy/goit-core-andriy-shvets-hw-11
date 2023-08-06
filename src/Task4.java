4f2d76bc10657b74bcba68780fa8588cdd236158
Andriy Shvets <andriy2.shvets@gmail.com>
2023-08-06 23:42:53 +0300

    second commit

--- src/Task4.java
+++ src/Task4.java
@@ -1,5 +1,22 @@
-public class Task4 {
-    public static void main(String[] args) {
 
+import java.util.stream.Stream;
+
+public class Task4 {
+
+    public static void main(String[] args) {
+        long a = 25214903917L;
+        long c = 11L;
+        long m = (long) Math.pow(2, 48);
+        long seed = 0;
+
+        Stream<Long> randomLongNumbers = Stream.iterate(seed, (n) -> linearCongruentGenerator(a, c, m, n));
+        randomLongNumbers
+                .limit(10)
+                .peek(System.out::println)
+                .count();
+    }
+
+    static long linearCongruentGenerator (long a, long c, long m, long randomNumber) {
+        return (a * randomNumber + c) % m;
     }
 }
